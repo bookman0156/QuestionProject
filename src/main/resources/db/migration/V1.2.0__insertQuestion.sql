c5df3154838e69b73aad6cabf61d53d847e4859a
micshi <micshiserver@gmail.com>
2021-06-30 13:28:41 +0900

    マイグレーションの誤字を修正

--- src/main/resources/db/migration/V1.2.0__insertQuestion.sql
+++ src/main/resources/db/migration/V1.2.0__insertQuestion.sql
@@ -12,7 +12,7 @@ VALUES
 	("ORDER BY の説明として正しいのはどれか ", "特定のデータを取得する ", "重複するデータを除く ", "データを並び替える", "データをグループ化する", "データを並び替える", "SQL"),
 	("リンクを作成する場合、正しいものはどれか", "<p href=”https://oplan.co.jp/”>オープラン株式会社</p>", "<a href=”https://oplan.co.jp/”>オープラン株式会社</a>", "<img src=”https://oplan.co.jp/”>オープラン株式会社</img> ", "<h6 =”https://oplan.co.jp/”>オープラン株式会社</h6>", "<a href=”https://oplan.co.jp/”>オープラン株式会社</a>", "HTML/CSS"),
 	("<ul>要素に関する説明として正しいものはどれか", "<ul>要素で囲むと、ブラウザのタブ上に現れる", "<ul>要素で囲むと、ブラウザに表示されない", "<ul>要素で囲むと、数字が連番でつく", "<ul>要素で囲むと、黒点が先頭につく", "<ul>要素で囲むと、黒点が先頭につく", "HTML/CSS"),
-	("背景画像を載せるときの正しいプロパティを選択しなさい ", "background-color", "background-color-position", "img", "background-image", "backgroound-image", "HTML/CSS"),
+	("背景画像を載せるときの正しいプロパティを選択しなさい ", "background-color", "background-color-position", "img", "background-image", "background-image", "HTML/CSS"),
 	("一回のmarginですべて違う値で指定するときの順番を選択しなさい   (例：margin: 1px 2px 3px 4px;) ", "上右下左", "右上下左", "右左上下", "下上左右", "上右下左", "HTML/CSS"),
 	("displayプロパティで領域そのものを表示しない値を選択しなさい", "inline", "none", "block", "list-item", "none", "HTML/CSS"),
 	("不特定の数値や文字列などのデータが格納されたものの名前は？", "定数", "不定数", "変数", "浮動数", "変数", "PHP"),
